c8f6d080ed3af2d2090e008d3cb86a7bb3aa0575
Sparsh Bisen <sparshbisen123@gmail.com>
2024-04-24 20:37:05 +0530

    adding error filing and response filing

---
+++ src/utils/ApiError.js
@@ -0,0 +1,30 @@
+// to have a structure for getting the error we are using the Error class, this is present in node.js api error part.
+//  https://nodejs.org/api/errors.html
+// this is basically API Error filing
+
+class ApiError extends Error{
+// here Error is a class, have a look in the documentation.
+
+    constructor(
+        statuscode,
+        message="something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statuscode = statuscode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = this.errors
+
+        if(stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+
+}
+
+export { ApiError }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,38 @@
+// this is a general function which takes inpute and gives output accordingly, we have to talk to database multiple times so 
+// its better to make a generalized version
+// this is also known as utility function
+
+
+
+// THERE ARE 2 WAYS: USING PROMISES AND TRY&CATCH
+
+
+// THIS IS USING PORMISES
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error)=>next(error))
+    }
+}
+
+
+
+export { asyncHandler }
+
+
+// THIS IS USING TRY AND CATCH.
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(rew,res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+}
+*/
